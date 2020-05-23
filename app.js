20401858339721a668b3f4dbc078078389af6cf0
swethusiast <aitc.dev@gmail.com>
2020-05-23 02:16:12 +0200

    uploadImage

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 const mongoose = require('mongoose');
 const passport = require('passport');
-const authenticate = require('./authenticate');
 const config = require('./config');
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -55,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
